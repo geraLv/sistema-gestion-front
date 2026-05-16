4f60a8da10a742ed89d59e1dc65129eaa1f1d76a
gera-vega <gerardoluisvega@gmail.com>
2026-05-16 09:27:36 -0300

    se avilitaron las analiticas de vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
 import AppErrorBoundary from './components/AppErrorBoundary.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <AppErrorBoundary>
         <App />
+        <Analytics />
       </AppErrorBoundary>
     </QueryClientProvider>
   </StrictMode>,
